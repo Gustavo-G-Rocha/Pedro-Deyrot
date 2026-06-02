c19991fb2123bafd7eb8189c62020b101a52a325
Felipi Buettgen <felipicatarina@hotmail.com>
2026-06-02 12:07:12 -0300

    feat: update Firestore rules for public read access and remove AbaixoAssinado page

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import DenunciasLista from './pages/DenunciasLista';
 import DenunciaView from './pages/DenunciaView';
 import EventoView from './pages/EventoView';
 import LGPD from './pages/LGPD';
-import AbaixoAssinado from './pages/AbaixoAssinado.tsx';
 
 export default function App() {
   return (
@@ -37,9 +36,6 @@ export default function App() {
 
         {/* Página de Termos LGPD */}
         <Route path="/LGPD" element={<LGPD />} />
-
-        {/* Página de abaixo-assinado */}
-        <Route path="/abaixo-assinado" element={<AbaixoAssinado />} />
 
         {/* Rotas administrativas (sem Layout) */}
         <Route path="/admin" element={<AdminLogin />} />
